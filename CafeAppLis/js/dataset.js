fbce5d75bca226b76776e0214021254ee848bd05
Igor Matheus <89890078+GomesM765@users.noreply.github.com>
2021-10-22 14:34:01 -0300

    Update dataset.js

--- CafeAppLis/js/dataset.js
+++ CafeAppLis/js/dataset.js
@@ -1,34 +1,41 @@
 const itens = [
+  /* Abaixo temos as listas criada em jason, associado a variavel itens*/
   {
-    "imagem":"https://images.unsplash.com/flagged/photo-1552938218-ac9ec40c702a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
-    "nome":"Café",
-    "preparo":"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    imagem:
+      'https://images.unsplash.com/flagged/photo-1552938218-ac9ec40c702a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
+    nome: 'Café',
+    preparo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
-    "imagem":"https://images.unsplash.com/photo-1553909489-cd47e0907980?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80",
-    "nome":"Sandwiche",
-    "preparo":"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    imagem:
+      'https://images.unsplash.com/photo-1553909489-cd47e0907980?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80',
+    nome: 'Sandwiche',
+    preparo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
-    "imagem":"https://images.unsplash.com/photo-1598215439218-f79b46925d3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
-    "nome":"Suco",
-    "preparo":"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    imagem:
+      'https://images.unsplash.com/photo-1598215439218-f79b46925d3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80',
+    nome: 'Suco',
+    preparo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
-    "imagem":"https://images.unsplash.com/photo-1579306194872-64d3b7bac4c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1005&q=80",
-    "nome":"Bolo",
-    "preparo":"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    imagem:
+      'https://images.unsplash.com/photo-1579306194872-64d3b7bac4c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1005&q=80',
+    nome: 'Bolo',
+    preparo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
-    "imagem":"https://images.unsplash.com/photo-1600431562968-ef337c8733ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
-    "nome":"Biscoito",
-    "preparo":"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    imagem:
+      'https://images.unsplash.com/photo-1600431562968-ef337c8733ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+    nome: 'Biscoito',
+    preparo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
-    "imagem":"https://images.unsplash.com/photo-1559598467-f8b76c8155d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1351&q=80",
-    "nome":"Leite",
-    "preparo":"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    imagem:
+      'https://images.unsplash.com/photo-1559598467-f8b76c8155d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1351&q=80',
+    nome: 'Leite',
+    preparo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
 ];
-
+/* Exporta a itens, onde foi criada a lista acima */
 export default itens;
