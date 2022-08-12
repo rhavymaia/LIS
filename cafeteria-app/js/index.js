00a0320d3b878e99c7cd2b443f771fd73cf3c8ab
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2022-08-12 08:47:04 -0300

    Atualização para a turma de LS 2022.1

--- cafeteria-app/js/index.js
+++ cafeteria-app/js/index.js
@@ -1,24 +1,39 @@
 import itens from "./dataset.js";
 
-for (let item of itens) {
-  /*
-    let cardItem =
-      '<div class="col-4">' +
-      '<div class="card" style="width: 18rem;"></div>' +
-      '     <img src="' + item.imagem + '" class="card-img-top" alt="Xícara de café">';
-    */
-  let cardItem = `<div class="col-4">
+function createFoodCard(food) {
+  let foodCard = `<div class="col-4">
         <div class="card" style="width: 18rem;">
-            <img src="${item.imagem}" class="card-img-top" alt="${item.nome}">
+            <img src="${food.imagem}" class="card-img-top" alt="${food.nome}">
             <div class="card-body">
-                <h5 class="card-title">${item.nome}</h5>
+                <h5 class="card-title">${food.nome}</h5>
                 <p class="card-text">
-                    ${item.descricao}
+                    ${food.descricao}
                 </p>
             </div>
         </div>
     </div>`;
-
-  let cardDeck = document.getElementById("card-deck");
-  cardDeck.insertAdjacentHTML("beforeend", cardItem);
+  return foodCard;
 }
+
+function addFoodsCard() {
+  for (let food of itens) {
+    let foodCard = createFoodCard(food);
+    let cardDeck = document.getElementById("card-deck");
+    cardDeck.insertAdjacentHTML("beforeend", foodCard);
+  }
+}
+
+// Adicionar onclick no button: addFoodButton
+let addFoodButton = document.getElementById("addFoodButton");
+addFoodButton.onclick = (event) => {
+  // Recuperar o elemento do formulário pelo id.
+  let foodForm = document.getElementById("foodForm");
+
+  // Adicionar onsubmit no form: foodForm
+  foodForm.onsubmit = (e) => {
+    e.preventDefault();
+    //TODO: Manipular o formulário
+  };
+};
+
+addFoodsCard();
